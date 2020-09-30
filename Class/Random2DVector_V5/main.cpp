d7b648d46f579c155ec7457d211271b05f30a58f
Mark Lehr <mark.lehr@rcc.edu>
2020-09-30 09:40:47 -0700

    Signed-off-by: Mark Lehr <mark.lehr@rcc.edu>

--- Class/Random2DVector_V5/main.cpp
+++ Class/Random2DVector_V5/main.cpp
@@ -19,7 +19,6 @@ using namespace std;
 
 //Function Prototypes
 void filVc2D(vector<vector<int>> &,int,int);
-void filVc2D(vector<vector<int>> &,int);
 void prtVc2D(const vector<vector<int>> &);
 
 //Execution of Code Begins Here
@@ -50,7 +49,7 @@ int main(int argc, char** argv) {
     
     //Add columns to the augmented array
     cols=6;
-    filVc2D(array,cols);
+    filVc2D(array,0,cols);
     
     //Display the array
     cout<<"Augmented Columns of previous 2D Vector"<<endl;
@@ -71,25 +70,15 @@ void prtVc2D(const vector<vector<int>> &array){
     cout<<endl;
 }
 
-void filVc2D(vector<vector<int>> &array,int cols){
-    //Set min size
-    cols=cols<2?2:cols;
-    //Fill the array with random 2 Digit numbers
-    for(int row=0;row<array.size();row++){
-        for(int col=0;col<cols;col++){
-            array[row].push_back(rand()%90+10);
-        }
-    }
-}
-
 void filVc2D(vector<vector<int>> &array,int rows,int cols){
-    //Set min size
-    rows=rows<2?2:rows;
-    cols=cols<2?2:cols;
     //Fill the array with random 2 Digit numbers
-    int initSz=array.size();
-    for(int row=initSz;row<initSz+rows;row++){
-        array.push_back(vector<int>());
+    //Add the number of rows and columns to the array vector
+    //If row==0 then just add columns to the existing array
+    int begin=array.size();
+    if(rows==0)begin=0;
+    int end=array.size()+rows;
+    for(int row=begin;row<end;row++){
+        if(rows!=0)array.push_back(vector<int>());
         for(int col=0;col<cols;col++){
             array[row].push_back(rand()%90+10);
         }
